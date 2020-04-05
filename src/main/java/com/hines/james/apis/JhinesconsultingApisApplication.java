624709c73b3793e119697079ec84196e79f37e60
James Hines <jameshines10@gmail.com>
2020-04-05 13:49:52 -0500

    Initial commit

---
+++ src/main/java/com/hines/james/apis/JhinesconsultingApisApplication.java
@@ -0,0 +1,13 @@
+package com.hines.james.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhinesconsultingApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhinesconsultingApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hines/james/apis/JhinesconsultingApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hines.james.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhinesconsultingApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
